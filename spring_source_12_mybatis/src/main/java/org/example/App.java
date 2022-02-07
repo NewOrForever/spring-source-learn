ac7edd82f0e5975eeda46957eddc121a2de06505
NewOrForever <bruce1203@126.com>
2022-02-07 21:47:41 +0800

    手写spring整合mybatis

--- spring_source_12_mybatis/src/main/java/org/example/App.java
+++ spring_source_12_mybatis/src/main/java/org/example/App.java
@@ -19,7 +19,18 @@ public class App
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
 
+        /**
+         * FactoryBean创建bean知识点：
+         *  容器初始化创建非懒加载单例bean的时候如果当前遍历到的bean是factoryBean则会用&xxx去getBean，拿到的是FactoryBean实例对象
+         *  除非当前bean实现的是SmartFactoryBean,且实现的方法isEagerInit返回true，那么它就会在容器初始化的时候继续拿xxx去getBean，此时拿的是getObject
+         *  否则就只能在下次getBean("xxx")的时候才能拿到getObject
+         * FactoryBean依赖注入知识点（找UserMapper类型的beanName）：
+         *
+         */
 
+        // 这段代码其实就是构造一个beanName为userMapper的FactoryBean，UseService的UserMapper属性填充的时候UserMapper和FactoryBean的getObjectType
+        // 方法返回值相同就能执行getObject()方法返回值就是属性值
+        // 通过beandefinition设置构造方法参数值（通过构造方法来给属性赋值）
         AbstractBeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition().getBeanDefinition();
         beanDefinition.setBeanClass(MyFactoryBean.class);
         beanDefinition.getConstructorArgumentValues().addGenericArgumentValue(UserMapper.class);
--- spring_source_12_mybatis/src/main/java/org/example/mybatis/spring/MyFactoryBean.java
+++ spring_source_12_mybatis/src/main/java/org/example/mybatis/spring/MyFactoryBean.java
@@ -1,6 +1,7 @@
 package org.example.mybatis.spring;
 
 import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -14,15 +15,23 @@ import java.lang.reflect.Proxy;
  * @Date:2022/2/7 16:29
  * @Author:qs@1.com
  */
+@Component
 public class MyFactoryBean implements FactoryBean {
 
     private Class mapperInterface;
+
+    // 构造方法注入
+    // beandefinition添加构造方法的参数值
+    public MyFactoryBean(Class mapperInterface) {
+        this.mapperInterface = mapperInterface;
+    }
 
     @Override
     public Object getObject() throws Exception {
         return Proxy.newProxyInstance(mapperInterface.getClassLoader(), new Class[]{mapperInterface}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("this is myfactorybean getobject");
                 return null;
             }
         });
@@ -34,10 +43,4 @@ public class MyFactoryBean implements FactoryBean {
     }
 
 
-    public Class getMapperInterface() {
-        return mapperInterface;
-    }
-    public void setMapperInterface(Class mapperInterface) {
-        this.mapperInterface = mapperInterface;
-    }
 }
