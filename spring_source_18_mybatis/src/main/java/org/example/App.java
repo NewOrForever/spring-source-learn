b56c4846ebf109f7d1d3a95150b19f6ff8484da5
q s <1258920697@qq.com>
2022-03-09 16:58:27 +0800

    mybatis源码学习

--- spring_source_18_mybatis/src/main/java/org/example/App.java
+++ spring_source_18_mybatis/src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.example.entity.User;
+import org.mybatis.spring.SqlSessionFactoryBean;
 
 import java.io.IOException;
 import java.io.Reader;
--- spring_source_18_mybatis/src/test/java/org/example/AppTest.java
+++ spring_source_18_mybatis/src/test/java/org/example/AppTest.java
@@ -3,21 +3,18 @@ package org.example;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.ibatis.io.Resources;
+import org.example.entity.User;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     @Test
     public void testDate() {
         System.out.println(new Date());
@@ -26,6 +23,7 @@ public class AppTest
 
     @Test
     public void testTryResource() throws IOException {
+
         Properties props = new Properties();
 
         // io流原本要try-catch-finally，finally中要去close资源，现在只需要try带参数的简易写法
@@ -39,4 +37,28 @@ public class AppTest
             System.out.println(entry.getKey() + "->" + entry.getValue());
         }
     }
+
+    @Test
+    public void testUnmodifyList() {
+        List<String> originList = new LinkedList<>();
+        originList.add("a");
+        originList.add("b");
+        originList.add("c");
+
+        /**
+         * 用法：一个类维护了一个List属性（有get方法），拿到这个List后为了不让该list被修改只能是只读，所以让该get方法里面返回的是只读视图
+         */
+
+        // 返回的是originList的只读视图
+        List<String> unmodList = Collections.unmodifiableList(originList);
+        //unmodList.add("d"); // 会报错
+    }
+
+    @Test
+    public void testMapnull() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(null, "hahah");
+    }
+
+
 }
