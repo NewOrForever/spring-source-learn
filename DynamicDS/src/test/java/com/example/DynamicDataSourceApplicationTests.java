464a43f9c49bc1ca19cfd183e3c2b810cdca37ec
NewOrForever <bruce1203@126.com>
2022-04-09 09:27:51 +0800

    手写简单动态数据源切换

--- DynamicDS/src/test/java/com/example/DynamicDataSourceApplicationTests.java
+++ DynamicDS/src/test/java/com/example/DynamicDataSourceApplicationTests.java
@@ -1,10 +1,20 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class DynamicDataSourceApplicationTests {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void dynamicTest(){
+        
+    }
 
     @Test
     void contextLoads() {
